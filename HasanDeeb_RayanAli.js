196f2dbcb9fb8b639b45e245b53e1780e9e4fb13
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-12 16:19:32 +0300

    Exercis 4 completed

--- HasanDeeb_RayanAli.js
+++ HasanDeeb_RayanAli.js
@@ -40,10 +40,27 @@
 
  //Ex3
 
-for(let i=0;i<6;i++){ // first loop is for lines
-    // we have to log * as many as the number of line , for ex in the second line we should have two * .
-    for(let j=0;j<i+1;j++){// second loop is for columns 
-        console.log("*");
+// for(let i=0;i<6;i++){ // first loop is for lines
+//     // we have to log * as many as the number of line , for ex in the second line we should have two * .
+//     for(let j=0;j<i+1;j++){// second loop is for columns 
+//         console.log("*");
+//     }
+//     console.log("\n");// after each column loop finish we have to jump into new line 
+// }
+
+//Ex4
+var array = [1,301,9,100,2,200,4] // list for testing
+var max = array[0] // initialized a max var to store the biggest number
+// nested loops, first one to iterate from the start till the middle, and the loop inside of it will check rom the last to the middle
+for(var i = 1; i < array.length / 2; i++){
+    if(array[i] > max){
+        max = array[i] //store the current index item if its bigger than the current biggest number
     }
-    console.log("\n");// after each column loop finish we have to jump into new line 
+    for(var j = array.length -1 ; j >= array.length / 2 ;j--){
+        if(array[j] > max){
+            max = array[j] //store the current index item if its bigger than the current biggest number
+        }
+    }
+
 }
+console.log("The largest number is "+max);
